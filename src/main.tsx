74d8b2c1979ef739490e8db9d0f9d7605cb4c817
Daryl Stark <daryl@dstark.nl>
2024-05-27 21:33:47 +0200

    Started styling the loginform

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Layout from './layout/Layout.tsx'
-import './index.css'
+import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
