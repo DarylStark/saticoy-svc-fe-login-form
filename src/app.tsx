d1a1e91b01d5568eed8c8a1a68e1f46ada39a0e8
Daryl Stark <daryl@dstark.nl>
2024-05-31 22:39:52 +0200

    Better styling

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,13 @@ import { useState } from 'react';
 import LoginPage from './components/templates/login_page';
 
 import { ConfigProvider } from 'antd';
-
-import light_theme from './saticoy-themes/light';
-import dark_theme from './saticoy-themes/dark';
-
 import './index.scss'
+
+import light_theme from './themes/light';
+import dark_theme from './themes/dark';
+
+import saticoy_theme from './themes/saticoy';
+
 
 function App() {
     const [is_dark_mode, set_dark_mode] = useState(false);
@@ -15,7 +17,7 @@ function App() {
     const toggle_theme = () => set_dark_mode(!is_dark_mode);
 
     return (
-        <ConfigProvider theme={is_dark_mode ? dark_theme : light_theme}>
+        <ConfigProvider theme={is_dark_mode ? saticoy_theme.dark.antd : saticoy_theme.light.antd}>
             <LoginPage toggle_theme={toggle_theme} />
         </ConfigProvider>
     );
