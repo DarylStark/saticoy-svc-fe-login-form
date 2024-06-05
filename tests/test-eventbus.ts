b05d66f23dd793f57876003f114dbc06948f9218
Daryl Stark <daryl@dstark.nl>
2024-06-05 21:46:15 +0200

    Increased code quality

--- tests/test-eventbus.ts
+++ tests/test-eventbus.ts
@@ -10,36 +10,27 @@ describe('EventBus', () => {
     });
 
     it('Adding subscribers', () => {
-        const callback = () => {
-            call_count++;
-        }
+        const callback = () => call_count++;
         event_bus.on('test_event', callback);
         expect(event_bus.subscriptions['test_event'].length).toBe(1);
     });
 
     it('Adding the same', () => {
-        const callback = () => {
-            call_count++;
-        }
+        const callback = () => call_count++;
         event_bus.on('test_event', callback);
         event_bus.on('test_event', callback);
         expect(event_bus.subscriptions['test_event'].length).toBe(1);
     });
 
     it('Deleting subscribers', () => {
-        const callback = () => {
-            call_count++;
-        }
+        const callback = () => call_count++;
         event_bus.on('test_event', callback);
         event_bus.off('test_event', callback);
         expect(event_bus.subscriptions['test_event'].length).toBe(0);
     });
 
     it('Raising events', () => {
-        event_bus.on('test_event', () => {
-            call_count++;
-        });
-
+        event_bus.on('test_event', () => call_count++);
         event_bus.raise('test_event', 'testData');
         expect(call_count).toBe(1);
     });
