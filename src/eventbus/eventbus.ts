0b5173f01a5f13a974fc92315ac67fd142b65776
Daryl Stark <daryl@dstark.nl>
2024-06-05 21:38:09 +0200

    Add more features to the `EventBus`

--- src/eventbus/eventbus.ts
+++ src/eventbus/eventbus.ts
@@ -1,17 +1,25 @@
-interface Event {
-    event: string;
-    data: any;
-}
+type EventHandlerCallback = (event: string, data: any) => void;
+type Subscriptions = { [key: string]: EventHandlerCallback[] };
 
 class EventBus {
-    constructor(public listeners: Event[] = []) {
+    constructor(public subscriptions: Subscriptions = {}) {
     }
 
-    subscribe(event: string, callback: (data: any) => void) {
-        this.listeners.push({
-            event,
-            data: callback
-        });
+    on(event: string, callback: EventHandlerCallback) {
+        this.subscriptions[event] = this.subscriptions[event] || [];
+        if (!this.subscriptions[event].includes(callback))
+            this.subscriptions[event].push(callback);
+    }
+
+    off(event: string, callback: EventHandlerCallback) {
+        this.subscriptions[event] = (this.subscriptions[event] || [])
+            .filter(sub => sub !== callback);
+    }
+
+    raise(event: string, data: any) {
+        for (const callback of this.subscriptions[event] || []) {
+            callback(event, data);
+        }
     }
 }
 
--- tests/test-eventbus.ts
+++ tests/test-eventbus.ts
@@ -2,22 +2,53 @@ import EventBus from '../src/eventbus/eventbus';
 
 describe('EventBus', () => {
     let eventBus: EventBus;
+    let call_count: number;
 
     beforeEach(() => {
         eventBus = new EventBus();
-    });
-
-    it('Instance type', () => {
-        expect(eventBus).toBeInstanceOf(EventBus);
+        call_count = 0;
     });
 
     it('Adding subscribers', () => {
-        const callback = jest.fn();
-        eventBus.subscribe('testEvent', callback);
+        const callback = (event: string, data: any) => {
+            console.log(`EventBus: raising event ${event}`);
+            console.log(`EventBus: data: ${data}`);
+            call_count++;
+        }
+        eventBus.on('test_event', callback);
+        expect(eventBus.subscriptions['test_event'].length).toBe(1);
+    });
 
-        expect(eventBus.listeners).toContainEqual({
-            event: 'testEvent',
-            data: callback
+    it('Adding the same', () => {
+        const callback = (event: string, data: any) => {
+            console.log(`EventBus: raising event ${event}`);
+            console.log(`EventBus: data: ${data}`);
+            call_count++;
+        }
+        eventBus.on('test_event', callback);
+        eventBus.on('test_event', callback);
+        expect(eventBus.subscriptions['test_event'].length).toBe(1);
+    });
+
+    it('Deleting subscribers', () => {
+        const callback = (event: string, data: any) => {
+            console.log(`EventBus: raising event ${event}`);
+            console.log(`EventBus: data: ${data}`);
+            call_count++;
+        }
+        eventBus.on('test_event', callback);
+        eventBus.off('test_event', callback);
+        expect(eventBus.subscriptions['test_event'].length).toBe(0);
+    });
+
+    it('Raising events', () => {
+        eventBus.on('test_event', (event: string, data: any) => {
+            console.log(`EventBus: raising event ${event}`);
+            console.log(`EventBus: data: ${data}`);
+            call_count++;
         });
+
+        eventBus.raise('test_event', 'testData');
+        expect(call_count).toBe(1);
     });
 });
