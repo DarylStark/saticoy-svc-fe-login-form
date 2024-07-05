5c0e241dcb1d5bea89cf968f3aa7bbf22de88300
Daryl Stark <daryl@dstark.nl>
2024-07-05 13:51:37 +0200

    Add i18n support for English translation

--- src/components/organisms/login_form.tsx
+++ src/components/organisms/login_form.tsx
@@ -2,15 +2,18 @@ import { Card } from 'antd'
 import { Button, Input, Typography } from "antd";
 import { FaRegUser, FaLock } from "react-icons/fa";
 import './login_form.scss'
+import { useTranslation } from 'react-i18next';
+
 
 const { Title, Paragraph } = Typography;
 
 function LoginForm() {
+    const { t } = useTranslation();
     return <>
         <Card className='card'>
             <form>
                 <Title level={3}>Welcome back!</Title>
-                <Paragraph>Sign in to continue</Paragraph>
+                <Paragraph>{t('welcome')}</Paragraph>
                 <Input placeholder='Username' prefix={<FaRegUser />} size='large' />
                 <Input.Password placeholder='Password' prefix={<FaLock />} size='large' />
                 <Button type='primary'>Login</Button>
---
+++ src/i18n.ts
@@ -0,0 +1,25 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+    .use(HttpApi)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'en',
+        debug: true,
+        interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+        },
+        backend: {
+            loadPath: '/locales/{{lng}}/{{ns}}.json',
+        },
+    });
+
+i18n.on('languageChanged', (lng) => {
+    console.log('Language changed to:', lng);
+});
+
+export default i18n;
