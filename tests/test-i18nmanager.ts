488d161fab4bd37cdba3c91ce0b319d751d5d151
Daryl Stark <daryl@dstark.nl>
2024-07-22 21:12:18 +0200

    Update tests

--- tests/test-i18nmanager.ts
+++ tests/test-i18nmanager.ts
@@ -1,51 +1,51 @@
 import I18nManager, { BaseI18nManager } from '../src/internationalization/i18n-manager';
-import { Language } from '../src/internationalization/language';
+import { LocaleData } from '../src/internationalization/localedata';
 import { BaseRepository } from '../src/repository/repository';
 import I18nStrategy from '../src/internationalization/i18n-strategy';
 
-const mock_language_en: Language = { languageCode: 'en' };
-const mock_language_nl: Language = { languageCode: 'nl' };
-const mock_language_de: Language = { languageCode: 'de' };
+const mock_language_en: LocaleData = { identifier: 'en' };
+const mock_language_nl: LocaleData = { identifier: 'nl' };
+const mock_language_de: LocaleData = { identifier: 'de' };
 
-class WorkingLanguageSelector implements I18nStrategy {
-    getLanguage(): string | undefined {
+class WorkingStrategy implements I18nStrategy {
+    getLocaleKey(): string | undefined {
         return 'en';
     }
 }
 
-class NotWorkingLanguageSelector implements I18nStrategy {
-    getLanguage(): string | undefined {
+class NotWorkingStrategy implements I18nStrategy {
+    getLocaleKey(): string | undefined {
         return undefined;
     }
 }
 
-describe('I18nManager without initial LanguageManager', () => {
-    const my_repository = new BaseRepository<Language>();
+describe('I18nManager without initial strategy', () => {
+    const my_repository = new BaseRepository<LocaleData>();
     let my_manager: I18nManager;
 
     beforeEach(() => {
         my_repository.add(mock_language_en, 'en');
         my_repository.add(mock_language_nl, 'nl');
         my_repository.add(mock_language_de, 'de');
-        my_manager = new BaseI18nManager<Language>(my_repository);
+        my_manager = new BaseI18nManager<LocaleData>(my_repository);
     });
 
     it('Retrieve the language from the working selector', () => {
-        my_manager.setStrategy(new WorkingLanguageSelector());
-        my_manager.retrieveLanguage();
-        expect(my_manager.selectedLanguage).toBe('en');
+        my_manager.setStrategy(new WorkingStrategy());
+        my_manager.retrieveLocaleKey();
+        expect(my_manager.selectedLocaleKey).toBe('en');
     });
 
     it('Fail back to default language', () => {
-        my_manager.setStrategy(new NotWorkingLanguageSelector());
-        my_manager.setDefaultLanguage('de');
-        my_manager.retrieveLanguage();
-        expect(my_manager.selectedLanguage).toBe('de');
+        my_manager.setStrategy(new NotWorkingStrategy());
+        my_manager.setDefaultLocaleKey('de');
+        my_manager.retrieveLocaleKey();
+        expect(my_manager.selectedLocaleKey).toBe('de');
     });
 
     it('Fail if no default language is set and LanaguageSelector is faulty', () => {
-        my_manager.setStrategy(new NotWorkingLanguageSelector());
-        my_manager.retrieveLanguage();
-        expect(() => my_manager.selectedLanguage).toThrow();
+        my_manager.setStrategy(new NotWorkingStrategy());
+        my_manager.retrieveLocaleKey();
+        expect(() => my_manager.selectedLocaleKey).toThrow();
     });
 });
