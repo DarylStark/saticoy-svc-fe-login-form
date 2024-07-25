540ed32c8de6ac69f800488717ae6cc036c94791
Daryl Stark <daryl@dstark.nl>
2024-07-25 21:41:37 +0200

    Implement `ThemeRetrievers`

--- src/theme-manager/theme-retriever.ts
+++ src/theme-manager/theme-retriever.ts
@@ -1,6 +1,35 @@
 import { ThemeMode } from './theme';
 
-export default interface ThemeRetriever {
+interface ThemeRetriever {
     retrieveMode(): ThemeMode | undefined;
     retrieveTheme(): string | undefined;
 }
+
+class BrowserRetriever implements ThemeRetriever {
+    retrieveMode(): ThemeMode | undefined {
+        const mode = window.matchMedia('(prefers-color-scheme: dark)').matches ? ThemeMode.Dark : ThemeMode.Light;
+        return mode;
+    }
+
+    retrieveTheme(): string | undefined {
+        return undefined;
+    }
+}
+
+class LocalPreferencesRetriever implements ThemeRetriever {
+    constructor(private _key_theme: string, private _key_mode: string) { }
+
+    retrieveMode(): ThemeMode | undefined {
+        const mode = localStorage.getItem(this._key_mode);
+        if (mode === 'light') return ThemeMode.Light;
+        if (mode === 'dark') return ThemeMode.Dark;
+        return undefined;
+    }
+
+    retrieveTheme(): string | undefined {
+        return localStorage.getItem(this._key_theme) || undefined;
+    }
+}
+
+export default ThemeRetriever;
+export { BrowserRetriever, LocalPreferencesRetriever };
