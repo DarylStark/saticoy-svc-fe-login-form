c7798cd91223e4bac9a9c84d6bb10ab0bfe5bc3a
Daryl Stark <daryl@dstark.nl>
2024-07-25 22:23:05 +0200

    Update app

--- src/app.tsx
+++ src/app.tsx
@@ -4,13 +4,30 @@ import { useState } from 'react';
 // Styling
 import { ConfigProvider } from 'antd';
 import './index.scss'
+import { themeController } from './globals/theme';
 
 // Organism
 import LoginPage from './components/templates/login_page';
 
+const updateBodyClass = () => {
+    document.body.className = themeController.currentStyle?.page['class'] || '';
+}
+
+updateBodyClass();
+
 function App() {
+    const [theme, setTheme] = useState(themeController.currentStyle);
+    themeController.eventBus?.on('theme_changed', () => {
+        setTheme(themeController.currentStyle);
+        updateBodyClass();
+    });
+    themeController.eventBus?.on('mode_changed', () => {
+        setTheme(themeController.currentStyle);
+        updateBodyClass();
+    });
+
     return (
-        <ConfigProvider>
+        <ConfigProvider theme={theme?.antd}>
             <LoginPage />
         </ ConfigProvider>
     );
