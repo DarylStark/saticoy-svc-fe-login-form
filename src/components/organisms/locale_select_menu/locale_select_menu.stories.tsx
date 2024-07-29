0e24072e514e254bc19d3425b48072bffdcbc400
Daryl Stark <daryl@dstark.nl>
2024-07-29 15:34:48 +0200

    Make the menu work in Storybook

--- src/components/organisms/locale_select_menu/locale_select_menu.stories.tsx
+++ src/components/organisms/locale_select_menu/locale_select_menu.stories.tsx
@@ -1,4 +1,31 @@
+import LocaleData from "../../../internationalization/localedata";
+import { BaseRepository } from "../../../repository/repository";
 import LocaleSelectMenu from "./locale_select_menu";
+import I18nController from "../../../internationalization/i18n-controller";
+import EventBus from "../../../eventbus/eventbus";
+
+// Create a Locale Repository
+const LocaleRepo = new BaseRepository<LocaleData>();
+LocaleRepo.add(
+    {
+        identifier: 'en-US',
+    },
+    "en-US"
+);
+LocaleRepo.add(
+    {
+        identifier: 'nl-NL',
+    },
+    "nl-NL"
+);
+
+// Create a Theme Controller
+const i18nController = new I18nController<LocaleData>(
+    LocaleRepo,
+    new EventBus()
+);
+i18nController.isAutoLocale = true;
+i18nController.defaultLocale = 'en-US';
 
 // Default component
 export default {
@@ -6,6 +33,6 @@ export default {
     component: LocaleSelectMenu,
 };
 
-const Template = () => <LocaleSelectMenu />;
+const Template = () => <LocaleSelectMenu localeController={i18nController} />;
 
 export const Default = Template.bind({});
