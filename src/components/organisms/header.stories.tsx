3554c01d3af5e15ffbe72d6eb00cd30471f50ee5
Daryl Stark <daryl@dstark.nl>
2024-07-29 15:58:10 +0200

    Make the `ThemeSelectMenu` work in Storybook

--- src/components/organisms/header.stories.tsx
+++ src/components/organisms/header.stories.tsx
@@ -1,6 +1,7 @@
 // src/components/Button.stories.tsx
 import Header, { HeaderProps } from "./header";
 import i18nController from "./locale_select_menu/test_data";
+import themeController from "./theme_select_menu/test_data";
 
 export default {
     title: 'Organisms/Header',
@@ -14,8 +15,21 @@ NoMenus.args = {
     children: 'Header without menus',
 };
 
+export const WithThemeMenu = Template.bind({});
+WithThemeMenu.args = {
+    children: 'Header with theme menu',
+    themeController: themeController,
+};
+
 export const WithLocaleMenu = Template.bind({});
 WithLocaleMenu.args = {
     children: 'Header with locale menu',
+    localeController: i18nController,
+};
+
+export const WithThemeMenuAndLocaleMenu = Template.bind({});
+WithThemeMenuAndLocaleMenu.args = {
+    children: 'Header with theme and locale menu',
+    themeController: themeController,
     localeController: i18nController,
 };
