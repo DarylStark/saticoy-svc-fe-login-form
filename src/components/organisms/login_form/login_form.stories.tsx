17d58af4ffccd53ab92c9f66f90a835e8fca1485
Daryl Stark <daryl.stark@asml.com>
2024-07-30 13:37:01 +0200

    Add some styling to the LoginForm

--- src/components/organisms/login_form/login_form.stories.tsx
+++ src/components/organisms/login_form/login_form.stories.tsx
@@ -1,12 +1,17 @@
-import LoginForm from "./login_form";
+import LoginForm, {LoginFormProps} from "./login_form";
 
 export default {
     title: 'Molecules/Forms/LoginForm',
     component: LoginForm,
 };
 
-const Template = () => <LoginForm />;
+const Template = (props: LoginFormProps) => <LoginForm {...props} />;
 
 export const Default = Template.bind({});
 Default.args = {
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+    error: 'A error occured while logging in'
 };
--- src/components/organisms/login_form/login_form.tsx
+++ src/components/organisms/login_form/login_form.tsx
@@ -1,53 +1,76 @@
 import {
-    Text,
+    Alert,
+    AlertIcon,
+    Box,
+    Button,
     Card,
     CardBody,
     CardHeader,
+    FormControl,
+    FormLabel,
     Heading,
-    Button,
     Input,
     InputGroup,
     InputLeftElement,
-    FormControl,
-    FormLabel
+    Text,
+    VStack,
 } from '@chakra-ui/react'
 import { FaRegUser, FaLock } from "react-icons/fa";
 import { useTranslation } from 'react-i18next';
 
-function LoginForm() {
+interface LoginFormProps {
+    error?: string
+};
+
+function LoginForm(props: LoginFormProps) {
     const { t } = useTranslation();
     return <>
-        <Card variant='outline' size='lg'>
+        <Card variant='outline'>
             <CardHeader>
                 <Heading size='md'>{t('login_title')}</Heading>
             </CardHeader>
             <CardBody>
-                <Text>{t('tagline')}</Text>
+
+                {props.error &&
+                    <Alert status='error'>
+                        <AlertIcon />
+                        {props.error}
+                    </Alert>
+                }
+                
                 <form>
-                    <FormControl>
-                        <FormLabel>{t('username')}</FormLabel>
-                        <InputGroup>
-                            <InputLeftElement pointerEvents='none'>
-                                <FaRegUser />
-                            </InputLeftElement>
-                            <Input placeholder={t('username')} />
-                        </InputGroup>
-                    </FormControl>
-                    <FormControl>
-                        <FormLabel>{t('password')}</FormLabel>
-                        <InputGroup>
-                            <InputLeftElement pointerEvents='none'>
-                                <FaLock />
-                            </InputLeftElement>
-                            <Input placeholder={t('password')} type='password' />
-                        </InputGroup>
-                    </FormControl>
-                    <Button
-                        type='submit'
-                        colorScheme='teal'
-                    >
-                        {t('login')}
-                    </Button>
+                <VStack spacing={4}>
+                    <Box>
+                        <FormControl>
+                            <FormLabel>{t('username')}</FormLabel>
+                            <InputGroup>
+                                <InputLeftElement pointerEvents='none'>
+                                    <FaRegUser />
+                                </InputLeftElement>
+                                <Input placeholder={t('username')} />
+                            </InputGroup>
+                        </FormControl>
+                    </Box>
+                    <Box>
+                        <FormControl>
+                            <FormLabel>{t('password')}</FormLabel>
+                            <InputGroup>
+                                <InputLeftElement pointerEvents='none'>
+                                    <FaLock />
+                                </InputLeftElement>
+                                <Input placeholder={t('password')} type='password' />
+                            </InputGroup>
+                        </FormControl>
+                    </Box>
+                    <Box>
+                        <Button
+                            type='submit'
+                            colorScheme='teal'
+                        >
+                            {t('login')}
+                        </Button>
+                    </Box>
+                </VStack>
                 </form>
             </CardBody>
         </Card>
@@ -55,3 +78,4 @@ function LoginForm() {
 }
 
 export default LoginForm
+export type {LoginFormProps}
