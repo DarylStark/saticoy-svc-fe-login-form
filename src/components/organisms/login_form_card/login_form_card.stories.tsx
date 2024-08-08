9cb9f672e82bb9e423276a40242e5311db51f0d8
Daryl Stark <daryl@dstark.nl>
2024-08-08 09:01:32 +0200

    Fix the `LoginFormCard` stories

--- src/components/organisms/login_form_card/login_form_card.stories.tsx
+++ src/components/organisms/login_form_card/login_form_card.stories.tsx
@@ -1,4 +1,4 @@
-import LoginFormCard, { LoginFormCardProps } from "./login_form_card";
+import LoginFormCard, { LoginFormCardProps, LoginFormType } from "./login_form_card";
 
 export default {
     title: 'Organisms/LoginFormCard',
@@ -7,41 +7,12 @@ export default {
 
 const Template = (props: LoginFormCardProps) => <LoginFormCard {...props} />;
 
-// With Username and Password link
+// With Username and Password form
 
 export const UsernameAndPasswordDefault = Template.bind({});
 UsernameAndPasswordDefault.args = {
     text: 'Here comes the text to explain to the user what he needs to do.',
-    form: 1
-};
-
-export const UsernameAndPasswordWithWarning = Template.bind({});
-UsernameAndPasswordWithWarning.args = {
-    text: 'Here comes the text to explain to the user what he needs to do.',
-    warning: 'This is a warning!',
-    form: 1
-};
-
-export const UsernameAndPasswordWithError = Template.bind({});
-UsernameAndPasswordWithError.args = {
-    text: 'Here comes the text to explain to the user what he needs to do.',
-    error: 'This is an error!',
-    form: 1
-};
-
-export const UsernameAndPasswordWithInfo = Template.bind({});
-UsernameAndPasswordWithInfo.args = {
-    text: 'Here comes the text to explain to the user what he needs to do.',
-    info: 'This is information!',
-    form: 1
-};
-
-// With MFA TOTP form
-
-export const MFATOTPDefault = Template.bind({});
-MFATOTPDefault.args = {
-    text: 'Here comes the text to explain to the user what he needs to do.',
-    form: 2
+    form_type: LoginFormType.UsernameAndPassword
 };
 
 // With Magic Code form
@@ -49,5 +20,21 @@ MFATOTPDefault.args = {
 export const MagicCodeDefault = Template.bind({});
 MagicCodeDefault.args = {
     text: 'Here comes the text to explain to the user what he needs to do.',
-    form: 3
+    form_type: LoginFormType.MagicCode
+};
+
+// With Authorize from session form
+
+export const AuthorizeFromSessionDefault = Template.bind({});
+AuthorizeFromSessionDefault.args = {
+    text: 'Here comes the text to explain to the user what he needs to do.',
+    form_type: LoginFormType.AuthorizeFromSession
+};
+
+// With MFA TOTP form
+
+export const MFATOTPDefault = Template.bind({});
+MFATOTPDefault.args = {
+    text: 'Here comes the text to explain to the user what he needs to do.',
+    form_type: LoginFormType.MFATOTP
 };
