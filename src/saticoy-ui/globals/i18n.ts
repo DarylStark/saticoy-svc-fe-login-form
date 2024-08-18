c177c394c9c40151f593fa7b99b6a83570bd82a9
Daryl Stark <daryl@dstark.nl>
2024-08-18 14:21:19 +0200

    Better code structure

--- src/saticoy-ui/globals/i18n.ts
+++ src/saticoy-ui/globals/i18n.ts
@@ -8,6 +8,10 @@ import en_US from "../languages/en-US";
 import nl_NL from "../languages/nl-NL";
 import I18nController from "../../saticoy-core/internationalization/i18n-controller";
 
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import HttpApi from 'i18next-http-backend';
+
 // Repository
 const localeRepo = new BaseRepository<i18NextLocaleData>();
 localeRepo.add(en_US, "en-US", ["en"]);
@@ -26,4 +30,40 @@ i18nController.saver = new LocalPreferencesSaver("locale");
 i18nController.defaultLocale = "en-US";
 i18nController.retrieveLocaleAutomatically();
 
-export { i18nController, localeRepo };
+// Create the language specifications
+const resources: {
+    [key: string]: {
+        [key: string]: {
+            [key: string]: string | {
+                [key: string]: string
+            }
+        }
+    }
+} = {}
+
+// List with all installed locales
+i18nController.localeRepository.getNames(true).forEach(i18n_key => {
+    resources[i18n_key] = i18nController.localeRepository.get(i18n_key).i18next;
+});
+
+// Initialize the i18n instance
+i18n
+    .use(HttpApi)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: i18nController.defaultLocale,
+        debug: true,
+        interpolation: {
+            escapeValue: false,
+        },
+        lng: i18nController.selectedLocale,
+        resources: resources
+    });
+
+
+// Listen for changes in the locale
+i18nController.eventBus?.on('i18n_locale_changed', (key) => {
+    i18n.changeLanguage(key);
+});
+
+export { i18nController, localeRepo, i18n };
