e40f289883fe4dc2ddb2f5b0a938e2145750eae6
Daryl Stark <daryl@dstark.nl>
2024-08-20 21:46:30 +0200

    Rename page file
    Fixes #19
    Fixes #20
    Fixes #21

--- src/components/pages/login-page.tsx
+++ src/components/pages/login-page.tsx
@@ -1,8 +1,8 @@
 // Saticoy UI main component
-import SaticoyUI from '../../../saticoy-ui/components/saticoy-ui';
+import SaticoyUI from '../../saticoy-ui/components/saticoy-ui';
 
 // Template
-import LoginPageTemplate from '../../templates/login-page-template';
+import LoginPageTemplate from '../templates/login-page-template';
 
 // Translation
 import { useTranslation } from 'react-i18next';
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LoginPage from './components/pages/login-page/login-page'
+import LoginPage from './components/pages/login-page'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
